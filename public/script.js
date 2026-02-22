cd16fece72bbe1992792dcd91ec40618f4660541
zeldelatrio <zeldelatrio@gmail.com>
2026-02-22 23:30:21 +0800

    last nani auh

--- public/script.js
+++ public/script.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:3000/books";
+const API = "https://cpsulibrary.up.railway.app/books";
 
 // Load books when page loads
 window.addEventListener("DOMContentLoaded", loadBooks);
