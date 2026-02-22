237d357d526660854258b696660c28bb423a1c08
zeldelatrio <zeldelatrio@gmail.com>
2026-02-22 23:22:29 +0800

    perfect

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,22 +3,28 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Book } from './books/book.entity';
 import { BooksController } from './books/books.controller';
 import { BooksService } from './books/books.service';
+import { BooksModule } from './books/books.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      port: 3306,
-      username: 'root',
-      password: '', // change if you have password
-      database: 'cpsu_library',
-      entities: [Book],
-      synchronize: true, // auto create table (DEV only)
+      url: process.env.MYSQL_URL, 
+      autoLoadEntities: true,
+      synchronize: true, 
     }),
-         TypeOrmModule.forFeature([Book]), 
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+      serveRoot: '/',
+    }),
+    BooksModule,
   ],
-  controllers: [BooksController],
-  providers: [BooksService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
