a43d9b5f09678e3d83a9dc909328cb1370270183
zeldelatrio <zeldelatrio@gmail.com>
2026-02-22 23:26:44 +0800

    game

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { join } from 'path';
+import { readFileSync } from 'fs';
 
 @Injectable()
 export class AppService {
+
   getHello(): string {
-    return 'Hello World!';
+    return 'Hello from NestJS!';
+  }
+  
+  getIndexHtml(): string {
+    const pathToHtml = join(__dirname, '..', 'public', 'index.html');
+    return readFileSync(pathToHtml, { encoding: 'utf-8' });
   }
 }
 
